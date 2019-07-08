9208ad83edb3c81b956b30b04054adb568ae2e43
rivedroz23 <river.droz@gmail.com>
2019-07-08 15:41:47 -0700

    modified indexejs, styles and readMe

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Made by River Droz
 # What is TripTips?
 TripTips is a travel related app that allows you to check out the city that you are in (the app only covers 2 cities for now..) and see all the best places in town that other tourists have posted about! The app makes it a lot faster to find cool places to visit than searching Google or Yelp would be! No sponsored or random results.
 
+![TripTips diagram](img/triptipsdiagram.png)
+
+
 
 Public link to my App:
 
--- public/js/app.js
+++ public/js/app.js
@@ -83,8 +83,56 @@
         }, labelLayerId)
     });
 
+	/*map.on('load', function () {
+		// Add a layer showing the places.
+		map.addLayer({
+		"id": "places",
+		"type": "symbol",
+		"source": {
+		"type": "geojson",
+		"data": {
+		"type": "FeatureCollection",
+		"features": [{
+		"type": "Feature",
+		"properties": {
+		"description": "<strong>Make it Mount Pleasant</strong><p><a href=\"http://www.mtpleasantdc.com/makeitmtpleasant\" target=\"_blank\" title=\"Opens in a new window\">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
+		"icon": "theatre"
+		},
+		"geometry": {
+		"type": "Point",
+		"coordinates": [-122.333997,47.601342]
+		}
+	}, {
 
+		map.on('click', 'places', function (e) {
+			var coordinates = e.features[0].geometry.coordinates.slice();
+			var description = e.features[0].properties.description;
+			 
+			// Ensure that if the map is zoomed out such that multiple
+			// copies of the feature are visible, the popup appears
+			// over the copy being pointed to.
+			while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+			coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+			}
+			 
+			new mapboxgl.Popup()
+			.setLngLat(coordinates)
+			.setHTML(description)
+			.addTo(map);
+			});
+			 
+			// Change the cursor to a pointer when the mouse is over the places layer.
+			map.on('mouseenter', 'places', function () {
+			map.getCanvas().style.cursor = 'pointer';
+			});
+			 
+			// Change it back to a pointer when it leaves.
+			map.on('mouseleave', 'places', function () {
+			map.getCanvas().style.cursor = '';
+			});
+			});*/
 
+	
 
 
 // console.log('hello mapbox');
